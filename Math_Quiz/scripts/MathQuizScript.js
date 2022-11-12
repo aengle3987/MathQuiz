ee92278bc33fda08b5525012fd542a6b6a15576b
aengle3987 <aengle3987@gmail.com>
2022-11-12 00:18:41 -0500

    First commit

---
+++ Math_Quiz/scripts/MathQuizScript.js
@@ -0,0 +1,41 @@
+// Program Name: MathQuizScript
+// Program Purpose: Gives a 10 question math quiz to the user.
+// Programmer: Amy Engle
+// Date Created: 11/11/2022
+
+// This is a test comment
+
+function main()
+{
+	// Numeric Varibles
+	userChoice = 0;				// Stores user's choice
+	maxValue = 0;				// Stores max number for math problem
+	minValue = 0;				// Stores min number for math problem
+	
+	// String Varibles
+	userInput = "";				// Store user's input
+	anotherQuiz = "";			// Stores user's choice to take another quiz
+	
+	// Boolean Varibles
+	flag = 1;					// Flag to control loop
+	
+	// Constant Varibles
+	const EASY_QUIZ = 1;		// Stores "easy" quiz choice
+	const MED_QUIZ = 2;			// Stores "medium" quiz choice
+	const HARD_QUIZ = 3;		// Stores "hard" quiz choice
+	
+	const ADD_CHOICE = 1;		// Choice for addition quiz
+	const SUBTRACT_CHOICE = 2;	// Choice for subtraction quiz
+	const MULTIPLY_CHOICE = 3;	// Choice for multiplication quiz
+	const DIVIDE_CHOICE = 4;	// Choice for division quiz
+	
+	const EASY_MIN_VALUE = 1;	// Stores lowest number for "easy" quiz
+	const EASY_MAX_VALUE = 9;	// Stores highest number for "easy" quiz
+	
+	const MED_MIN_VALUE = 10;	// Stores lowest number for "medium" quiz
+	const MED_MAX_VALUE = 99;	// Stores highest number for "medium" quiz
+	
+	const HARD_MIN_VALUE = 100;	// Stores lowest number for "hard" quiz
+	const HARD_MAX_VALUE = 999;	// Stores highest number for "hard" quiz
+		  
+}
